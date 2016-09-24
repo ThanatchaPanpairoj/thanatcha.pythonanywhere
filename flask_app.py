e19330a1e9d7421ca106487497865fa870f0bd11
Thanatcha Panpairoj <therealthanatcha@gmail.com>
2016-09-24 02:53:57 +0000

    Linked MySQL database.

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,33 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="Thanatcha",
+    password="mysqlmysql",
+    hostname="Thanatcha.mysql.pythonanywhere-services.com",
+    databasename="Thanatcha$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+comments = ["Hello this is a page"]
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
