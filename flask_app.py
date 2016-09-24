cf41c47f08413d03267925922d77a506231195d4
Thanatcha Panpairoj <therealthanatcha@gmail.com>
2016-09-24 06:06:57 +0000

    Switched database on.

--- flask_app.py
+++ flask_app.py
@@ -22,12 +22,13 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-comments = ["Hello this is a page"]
-
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
+
