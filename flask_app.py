d81037060e8cfb18382c9918bd37cef3e8263a76
Thanatcha Panpairoj <therealthanatcha@gmail.com>
2016-09-24 00:56:21 +0000

    Moved files.

---
+++ flask_app.py
@@ -0,0 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def index():
+    return render_template("main_page.html")
